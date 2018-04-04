a77db584390931737c65c9f507285cb9f8664d9f
Arni Magnusson <arnima@hafro.is>
2018-04-04 12:47:31 +0200

    Update percentage example

--- R/icesRound.R
+++ R/icesRound.R
@@ -44,12 +44,12 @@
 #'
 #' ## Example from the ICES Technical Guidelines
 #' Actual <- c(0.35776, 0.34665, 0.202, 0.12665, 0.001567, 0.002567, 0.013415,
-#'             0.02315, 1.168, 2.15678, 9.546, 10.546, 23.445)
+#'             0.02315, 1.168, 2.15678)
 #' Rounded <- icesRound(Actual)
 #' print(data.frame(Actual=as.character(Actual), Rounded), row.names=FALSE)
 #'
-#' ## Example rounding percentage changes in SSB or TAC
-#' Actual <- c(-1.482, -9.09, 0.51, 130.11, 584)
+#' ## Continued example from Guidelines, now rounding percentages
+#' Actual <- c(9.546, 10.546, 23.445, -1.482, -9.09, 0.51, 130.11, 584)
 #' Rounded <- icesRound(Actual, percent = TRUE)
 #' print(data.frame(Actual=as.character(Actual), Rounded), row.names=FALSE)
 #'
