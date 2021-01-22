058d93f29684ec9577807708f24a08cbb881be87
Arni Magnusson <thisisarni@gmail.com>
2021-01-22 16:21:42 +0100

    Updated link to ICES advice

--- R/icesAdvice-package.R
+++ R/icesAdvice-package.R
@@ -43,6 +43,5 @@
 #' @author Arni Magnusson, Colin Millar, and Anne Cooper.
 #'
 #' @references
-#' ICES advisory process:
-#' \url{https://ices.dk/about-ICES/how-we-work/Pages/Advisory_process.aspx}
+#' ICES advice: \url{https://ices.dk/advice}
 NA
